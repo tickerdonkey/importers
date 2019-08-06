b509cdbffe1584e2cdc736ae05475b72f6d2ff26
Shawn Mclean <shawnrmclean@gmail.com>
2019-08-05 20:06:42 -0500

    Importer added to root.

---
+++ packages/exchange-importer/src/jse/index.ts
@@ -0,0 +1,14 @@
+import { ImportFilter } from '../common/ImportFilter'
+import { JSETickerImporter } from './JSETickerImporter'
+
+export default class JSEImporter {
+  private readonly tickerImporter: JSETickerImporter
+
+  constructor() {
+    this.tickerImporter = new JSETickerImporter()
+  }
+
+  public async importTickers(filter: ImportFilter) {
+    return this.tickerImporter.import(filter)
+  }
+}
