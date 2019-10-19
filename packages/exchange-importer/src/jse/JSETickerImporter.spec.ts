a3293d4601f7f82f3cc32915d5015f75b309f04d
Shawn Mclean <shawnrmclean@gmail.com>
2019-10-19 13:36:47 -0500

    Fix package.

--- packages/exchange-importer/src/jse/JSETickerImporter.spec.ts
+++ packages/exchange-importer/src/jse/JSETickerImporter.spec.ts
@@ -4,6 +4,7 @@ describe('ticker', () => {
   it('should get tickers by date', async () => {
     const sut = new JSETickerImporter()
     const tickers = await sut.import({
+      instrument: 'all-stocks',
       startDate: '2019-08-05',
       endDate: '2019-08-05',
     })
